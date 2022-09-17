c63bcd8d4817f046ec9b092a59b730e1e52f4a51
tur1ng <mohammadali.ap.2000@gmail.com>
2022-09-18 02:29:51 +0430

    fix(fire): python3

--- fire/py.go
+++ fire/py.go
@@ -3,5 +3,5 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func py(c *fiber.Ctx, execReq *ExecReq) error {
-	return execCmd(c, "python", execReq.Id)
+	return execCmd(c, "python3", execReq.Id)
 }
