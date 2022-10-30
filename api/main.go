09f4406262d38808ca5f04d37d9bb0f179e208d8
tur1ng <mohammadali.ap.2000@gmail.com>
2022-10-30 07:03:13 +0330

    feat(api): add swagger

--- api/main.go
+++ api/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/swagger"
 
+	_ "github.com/alawys-maap/yarcee/api/docs"
+	"github.com/gofiber/fiber/v2"
+)
 
+// @title          Fiber Example API
+// @version        1.0
+// @description    This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name   API Support
+// @contact.email  fiber@swagger.io
+// @license.name   Apache 2.0
+// @license.url    http://www.apache.org/licenses/LICENSE-2.0.html
+// @host           localhost:3000
+// @BasePath       /
 func main() {
-  app := fiber.New()
+	app := fiber.New()
+
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
