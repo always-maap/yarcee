51841403cb66d4c7b427aec7cb8b2ea594d29cf4
tur1ng <mohammadali.ap.2000@gmail.com>
2023-03-29 23:43:36 -0700

    fix(client): add props to logo

--- client/components/logo.tsx
+++ client/components/logo.tsx
@@ -1,6 +1,10 @@
-export default function Logo() {
+import { SVGAttributes } from 'react';
+
+type Props = {} & SVGAttributes<HTMLOrSVGElement>;
+
+export default function Logo(props: Props) {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g clipPath="url(#clip0_1_2)">
         <rect width="24" height="24" />
         <rect x="1.5" y="1.5" width="21" height="21" rx="10.5" stroke="white" strokeWidth="3" />
