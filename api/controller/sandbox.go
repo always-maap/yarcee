dbf301e378c21adb6b4bcdbf5c3a78ade0e1d3b1
tur1ng <mohammadali.ap.2000@gmail.com>
2023-04-17 03:08:40 +0330

    feat(api): sandbox crud

--- api/controller/sandbox.go
+++ api/controller/sandbox.go
@@ -6,6 +6,7 @@ import (
 	"api/model"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm/clause"
 )
 
 // @Summary      Get user sandboxes
@@ -45,7 +46,7 @@ type createSandboxBody struct {
 // @Produce      json
 // @Param request body createSandboxBody true "query params"
 // @Router       /api/sandbox/ [post]
-func CreateSandBox(c *fiber.Ctx) error {
+func CreateSandbox(c *fiber.Ctx) error {
 	var data = new(createSandboxBody)
 
 	if err := c.BodyParser(&data); err != nil {
@@ -71,4 +72,47 @@ func CreateSandBox(c *fiber.Ctx) error {
 	database.DB.Create(&sandbox)
 
 	return c.JSON(sandbox)
+}
+
+type updateSandboxBody struct {
+	Name     string `json:"name"`
+	Language string `json:"language"`
+	Code     string `json:"code"`
+}
+
+// @Summary      Update sandbox
+// @Tags         Sandbox
+// @Accept       json
+// @Produce      json
+// @Param request body updateSandboxBody true "query params"
+// @Router       /api/sandbox/ [put]
+func UpdateSandbox(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var data = new(updateSandboxBody)
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var sandbox model.Sandbox
+	database.DB.Model(&sandbox).Clauses(clause.Returning{}).Where("id = ?", id).Updates(model.Sandbox{Name: data.Name, Language: data.Language, Code: data.Code})
+
+	return c.JSON(&fiber.Map{
+		"message": "success",
+		"data":    sandbox,
+	})
+}
+
+// @Summary      Update sandbox
+// @Tags         Sandbox
+// @Accept       json
+// @Produce      json
+// @Router       /api/sandbox/ [delete]
+func DeleteSandbox(c *fiber.Ctx) error {
+	id := c.Params("id")
+	database.DB.Delete(&model.Sandbox{}, id)
+
+	return c.JSON(&fiber.Map{
+		"message": "success",
+	})
 }
--- api/router/sandbox.go
+++ api/router/sandbox.go
@@ -8,6 +8,8 @@ import (
 )
 
 func SandboxSetup(api fiber.Router) {
-	api.Post("/sandbox", middleware.Protected(), controller.CreateSandBox)
+	api.Post("/sandbox", middleware.Protected(), controller.CreateSandbox)
 	api.Get("/sandbox", middleware.Protected(), controller.GetUserSandboxes)
+	api.Put("/sandbox/:id", middleware.Protected(), controller.UpdateSandbox)
+	api.Delete("/sandbox/:id", middleware.Protected(), controller.DeleteSandbox)
 }
