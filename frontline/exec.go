41b47a38855327ade1cb4f7f92a1eb1ca1d905e7
tur1ng <mohammadali.ap.2000@gmail.com>
2023-07-26 23:17:40 +0330

    refactor(frontline): mem and exec duration

--- frontline/exec.go
+++ frontline/exec.go
@@ -4,9 +4,19 @@ import (
 	"bytes"
 	"net/http"
 	"os/exec"
+	"syscall"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+type ExecRes struct {
+	Message      string `json:"message"`
+	StdErr       string `json:"stderr"`
+	StdOut       string `json:"stdout"`
+	ExecDuration int64  `json:"exec_duration"`
+	MemUsage     int64  `json:"mem_usage"`
+}
 
 func execCmd(c *fiber.Ctx, prog string, args ...string) error {
 	var execStdOut, execStdErr bytes.Buffer
@@ -15,19 +25,25 @@ func execCmd(c *fiber.Ctx, prog string, args ...string) error {
 	cmd.Stdout = &execStdOut
 	cmd.Stderr = &execStdErr
 
+	start := time.Now()
 	err := cmd.Run()
+	elapsed := time.Since(start)
 
 	if err != nil {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
-			"error":  err.Error(),
-			"stdout": execStdOut.String(),
-			"stderr": execStdErr.String(),
+		return c.Status(http.StatusBadRequest).JSON(ExecRes{
+			Message:      "Failed to exec",
+			StdOut:       execStdOut.String(),
+			StdErr:       execStdErr.String(),
+			ExecDuration: elapsed.Milliseconds(),
+			MemUsage:     cmd.ProcessState.SysUsage().(*syscall.Rusage).Maxrss,
 		})
 	}
 
-	return c.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "OK",
-		"stdout":  execStdOut.String(),
-		"stderr":  execStdErr.String(),
+	return c.Status(http.StatusOK).JSON(ExecRes{
+		Message:      "Success",
+		StdOut:       execStdOut.String(),
+		StdErr:       execStdErr.String(),
+		ExecDuration: elapsed.Milliseconds(),
+		MemUsage:     cmd.ProcessState.SysUsage().(*syscall.Rusage).Maxrss,
 	})
 }
